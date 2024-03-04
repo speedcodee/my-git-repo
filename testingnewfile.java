a347038a83ff04bb2f711bdd48fd119fe6278476
Krihtik <kdham@wgu.edu>
2024-03-03 21:23:45 -0800

    Making more edits

--- testingnewfile.java
+++ testingnewfile.java
@@ -1,5 +1,11 @@
 public class testingnewfile {
     public static void main(String[] args) {
         System.out.println("Testing new file");
+
+        String word = "Adding an edit";
+        System.out.println(word);
+
+        String addOn = "More edits";
+        System.out.println(addOn);
     }
 }
